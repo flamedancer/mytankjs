558eac646eb12945b148b3014fc94d3311a1745a
admin <admin@longdeMacBook-Pro.local>
2017-01-22 17:46:44 +0800

    update

--- server.py
+++ server.py
@@ -4,81 +4,92 @@ import json
 import os
 import geventwebsocket
 
+GET_CONTROL_MSG = json.dumps(
+    {
+        'c': 'control',
+    }
+)
+
 all_players = []
 
 rooms = {}  # room_id: [p1, p2]
 
 def add_room_member(players, player):
-	origin_player = players[0]
-	players.append(player)
-	origin_player.partner = player
-	player.partner = origin_player
-	player.room_id = origin_player.room_id
+    origin_player = players[0]
+    players.append(player)
+    origin_player.partner = player
+    player.partner = origin_player
+    player.room_id = origin_player.room_id
 
-def get_game_model(player, room_id):
-	if room_id and room_id in rooms and len(rooms[room_id]) == 1:
-		add_room_member(rooms[room_id], player)
-		return 3
-	for room_id, players in rooms.items():
-		if len(players) == 1:
-			add_room_member(players, player)
-			return 3
-	else:
-		new_room_id = player.core_id
-		player.room_id = new_room_id
-		rooms[new_room_id] = [player]
-		return 2
+def get_is_control(player, room_id):
+    if room_id and room_id in rooms and len(rooms[room_id]) == 1:
+        add_room_member(rooms[room_id], player)
+        return False 
+    for room_id, players in rooms.items():
+        if len(players) == 1:
+            add_room_member(players, player)
+            return False 
+    else:
+        new_room_id = player.core_id
+        player.room_id = new_room_id
+        rooms[new_room_id] = [player]
+        return True 
 
 
 class Player(object):
-	def __init__(self, wc, core_id):
-		self.wc = wc
-		self.core_id = core_id
-		self.room_id = ''
-		self.partner = None
+    def __init__(self, wc, core_id):
+        self.wc = wc
+        self.core_id = core_id
+        self.room_id = ''
+        self.partner = None
 
-	def send_self(self, info):
-		msg = json.dumps(info) if not isinstance(info, (str)) else info
-		self.wc.send(msg)
+    def send_self(self, info):
+        msg = json.dumps(info) if not isinstance(info, (str)) else info
+        try:
+            self.wc.send(msg)
+        except:
+            print "partner close"
+            disconnect(self)
 
-	def send_partner(self, info):
-		msg = json.dumps(info) if not isinstance(info, (str)) else info
-		if self.partner:
-			self.partner.wc.send(msg)
+    def send_partner(self, info):
+        if self.partner:
+            self.partner.send_self(info)
 
-	def broad(self, info ):
-		msg = json.dumps(info)
-		self.send_self(msg)
-		self.send_partner(msg)
+    def broad(self, info ):
+        msg = json.dumps(info)
+        self.send_self(msg)
+        self.send_partner(msg)
 
-	def handler(self, msg):
-		info = json.loads(msg)
-		if info['c'] == 's':
-			# info['uuid'] = self.core_id
-			game_model = get_game_model(self, info.get('room_id'))
-			info['game_model'] = game_model
-			self.send_self(info)
-		elif info['c'] == 'req_init':
-			self.send_partner(info)
-		elif info['c'] == 'rsp_init':
-			self.send_partner(info)
-		else:
-			self.broad(info)
+    def handler(self, msg):
+        info = json.loads(msg)
+        if info['c'] == 's':
+            # info['uuid'] = self.core_id
+            is_control = get_is_control(self, info.get('room_id'))
+            info['is_control'] = is_control
+            self.send_self(info)
+        elif info['c'] == 'req_init':
+            self.send_partner(info)
+        elif info['c'] == 'rsp_init':
+            self.send_partner(info)
+        else:
+            self.broad(info)
 
 
 def app(environ, start_response):
     ws = environ.get("wsgi.websocket")
-    core_id = 11 #str(environ['HTTP_SEC_WEBSOCKET_KEY'])
+    core_id = str(environ['HTTP_SEC_WEBSOCKET_KEY'])
     player = Player(ws, core_id)
     all_players.append(player)
     print "connect: ", player.core_id
     try:
         while True:
-	        msg = ws.receive()
-	        player.handler(msg)
+            msg = ws.receive()
+            print core_id, "  :", msg
+            player.handler(msg)
     # except geventwebsocket.WebSocketError, ex:
     #     print "player left: ", player.core_id
-    except:
+    except Exception, e:
+        print e, e.message
         disconnect_player(player)
 
 
@@ -86,8 +97,11 @@ def disconnect_player(player):
     rooms[player.room_id].remove(player)
     if not rooms[player.room_id]:
         del rooms[player.room_id]
-	all_players.remove(player)
-	print "disconnect: ", player.core_id
+    all_players.remove(player)
+    print "disconnect: ", player.core_id
+    if player.partner and player.partner in all_players:
+        player.send_partner(GET_CONTROL_MSG)
+        player.partner.partner = None
 
 
 
