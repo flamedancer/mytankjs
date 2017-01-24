c5bd314d3c32136ef36fb47885769d7c0507e3b8
flamedancer <guochen070613@gmail.com>
2017-01-24 23:46:54 +0800

    update

--- control.js
+++ control.js
@@ -4,6 +4,8 @@ CONTROL = true; // 控制权
 SID_FLAG = 1;  // sid 符号位
 ROOM_INDEX = null;
 
+start_time = 0;
+
 function both(tank_name, pos) {
 	switch (GAME_MODEL) {
 		case 0:
@@ -68,18 +70,20 @@ function stop(tank) {
 }
 
 function start_game(game_model, room_id) {
+    var now = (new Date()).getTime(); // 毫秒
+    if ((now -start_time) <= 3000)
+        return
     GAME_MODEL = game_model;
-    if (ROOM_INDEX != null) {
-        send_leave_room();
-    }
-    BOTHS = {};
-    player = null;
-    ROOM_INDEX = null;
 	if (GAME_MODEL <= 1) {
-        get_control();
+        if (ROOM_INDEX != null) {
+            send_leave_room();
+        }
+        ROOM_INDEX = null;
+        CONTROL = true;
 		enter_stage("1");
         show_self_room();
     }
     else
         start(room_id);
+    start_time = now;
 }
